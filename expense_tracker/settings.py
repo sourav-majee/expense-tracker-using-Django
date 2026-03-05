17ec862b9e4decae148a0858fea4330383de7ad1
Sourav-majee <majeesourav.mca@email.com>
2026-03-05 14:35:04 +0530

    add render url

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-change-me-in-production")
 DEBUG      = os.getenv("DEBUG", "True") == "True"
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["https://expense-tracker-using-django-piwc.onrender.com", "expense-tracker-using-django-piwc.onrender.com"]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
