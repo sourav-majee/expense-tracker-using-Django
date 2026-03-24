0696427cdd8e5598e4adb6d40480ca6245ba6bd3
Sourav-majee <majeesourav.mca@email.com>
2026-03-24 13:39:09 +0530

    Fixed static files serving for PWA

--- expense_tracker/urls.py
+++ expense_tracker/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("expenses.urls")),
     path("", TemplateView.as_view(template_name="index.html"), name="home"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
